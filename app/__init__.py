4f29a15a6220624508bab0d488bbb089b4a7623d
Ibrahim Mbaziira <code.ibra@gmail.com>
2018-02-14 15:50:37 +0300

    [Fix] Fix cors

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask import redirect, Flask
 from flasgger import Swagger
+from flask.ext.cors import CORS
 from instance.config import app_config
-from flask_cors import CORS
+
 
 # initialize sql-alchemy
 db = SQLAlchemy()
